b5673f4ff9c5b618e6f2b4730819de73f5d1ba04
Ahmad Alzohaile <ahmad.alzohaile@gmail.com>
2024-11-08 06:34:02 +0100

    first commit

---
+++ src/main/java/com/example/gradle_unit_integration_test_demo/GradleUnitIntegrationTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle_unit_integration_test_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleUnitIntegrationTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleUnitIntegrationTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradle_unit_integration_test_demo/GradleUnitIntegrationTestDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.gradle_unit_integration_test_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleUnitIntegrationTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	void my_simple_unit_test() {
+	System.out.println("This is a unit test!");
+	}
+
+
+}
